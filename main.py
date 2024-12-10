0fd6839273d7899f5655425b7bba8571122fe307
muntasir ayan <mjayan439@gmail.com>
2024-12-10 18:19:39 +0600

    main.py added

---
+++ main.py
@@ -0,0 +1,44 @@
+import subprocess
+
+def run_script(script_name):
+    """Run a Python script using subprocess."""
+    try:
+        result = subprocess.run(['python', script_name], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(f"Successfully ran {script_name}")
+        print(result.stdout.decode())
+    except subprocess.CalledProcessError as e:
+        print(f"Error running {script_name}: {e.stderr.decode()}")
+
+def main():
+    print("H1 testing is running....")
+    run_script('h1Test.py')
+
+
+    print("HTML testing is running....")
+    run_script('htmlTest.py')
+
+
+    print("alt testing is running....")
+    run_script('altTest.py')
+
+
+    print("url testing is running....")
+    run_script('url_status_code_test.py')
+
+
+    print("Currency testing is running....")
+    run_script('currency.py')
+
+
+    print("Script testing is running....")
+    run_script('script.py')
+
+
+    print("Running successfull.")
+
+
+
+    
+
+if __name__ == "__main__":
+    main()
